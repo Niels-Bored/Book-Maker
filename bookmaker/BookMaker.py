1dd6a77e66e77286d140f7cb49483406c799a8f5
Stirl <stirling.butcher@gmail.com>
2023-09-12 13:11:11 +1000

    Update BookMaker.py

--- bookmaker/BookMaker.py
+++ bookmaker/BookMaker.py
@@ -45,7 +45,9 @@ def generatePDF(child_name, child_fullname, date, dedication):
 
 	c.showPage()
 
-	dedication="Dear Viewer you can add your hand written dedication here - or if you provide us your dedication message we will print it here for you."
+	dedication="Dear Viewer,
+ you can add your hand written dedication here - or if you provide us your dedication message we will print it here for you.
+BelovedCo Team"
 
 	wrapper = textwrap.TextWrapper(width=30)
 	word_list = wrapper.wrap(text = dedication)
